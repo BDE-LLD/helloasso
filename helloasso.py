39016cfbbe8569f89df4f2bbcc30d58dabbba43c
Saky <--replace-all>
2023-05-21 20:05:46 +0200

    v2 of helloasso.py

--- helloasso.py
+++ helloasso.py
@@ -1,33 +1,25 @@
 #! /usr/bin/env python3
 
+from dotenv import load_dotenv
+import os
+import requests
+import json
+import csv
 import sys
+
 from helloasso_api.oauth2 import OAuth2Api
 from helloasso_api.apiv5client import ApiV5Client
 
-clientId = ""
-clientSecret = ""
+load_dotenv()
+
+CLIENT_ID = os.getenv("HELLOASSO_CLIENT_ID")
+CLIENT_SECRET = os.getenv("HELLOASSO_CLIENT_SECRET")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 
 class MyApi:
     def __init__(self, client) -> None:
         self._client = client
-
-    def fetchPayments(self):
-        payments = {
-            "data": [],
-            "total": 0,
-        }
-        res = self._client.call("organizations/bde-42/forms/event/wed/payments").json()
-        total = res["pagination"]["totalCount"]
-        payments["total"] = total
-        payments["data"].extend(res["data"])
-        while total > 0:
-            total = res["pagination"]["totalCount"]
-            res = self._client.call(
-                f"organizations/bde-42/forms/event/wed/payments?continuationToken={res['pagination']['continuationToken']}"
-            ).json()
-            payments["data"].extend(res["data"])
-        return payments
 
     def fetchItems(self):
         items = {
@@ -47,33 +39,85 @@ class MyApi:
         return items
 
 
-auth = OAuth2Api(
-    "api.helloasso.com", client_id=clientId, client_secret=clientSecret, timeout=99999
-)
+def sendWebhook(data: dict):
+    try:
+        body = json.dumps(data)
+        res = requests.post(
+            WEBHOOK_URL,
+            json=data,
+            headers={"Content-Type": "application/json"},
+        )
+        if res.status_code != 204:
+            print(res.status_code)
+            print(res.text)
+    except Exception as e:
+        print(e)
 
-auth.get_token()
 
-client = ApiV5Client(
-    api_base="api.helloasso.com/v5/",
-    client_id=clientId,
-    client_secret=clientSecret,
-    access_token=auth.access_token,
-    refresh_token=auth.refresh_token,
-)
+def saveUsers(users: list):
+    with open("users.csv", "w") as f:
+        writer = csv.DictWriter(f, fieldnames=["firstName", "lastName"])
+        writer.writeheader()
+        writer.writerows(users)
 
-myApi = MyApi(client)
 
-res = list(filter(lambda x: x["type"] != "Donation", myApi.fetchItems()["data"]))
+def loadUsers():
+    users = []
+    with open("users.csv", "r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            users.append(row)
+    return users
 
-if len(sys.argv) != 2:
-    print("Usage: helloasso.py <total | list>")
-    sys.exit(1)
 
-if sys.argv[1] == "total":
-    print(len(res))
-elif sys.argv[1] == "list":
-    for item in res:
-        print(item["user"]["firstName"], item["user"]["lastName"])
-else:
-    print("Usage: helloasso.py <total | list>")
-    sys.exit(1)
+def main():
+    auth = OAuth2Api(
+        "api.helloasso.com",
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        timeout=99999,
+    )
+
+    auth.get_token()
+
+    client = ApiV5Client(
+        api_base="api.helloasso.com/v5/",
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        access_token=auth.access_token,
+        refresh_token=auth.refresh_token,
+    )
+
+    myApi = MyApi(client)
+
+    res = list(filter(lambda x: x["type"] != "Donation", myApi.fetchItems()["data"]))
+
+    users = [item["user"] for item in res]
+
+    arg = sys.argv[1]
+
+    match arg:
+        case "save":
+            saveUsers(users)
+        case "check":
+            savedUsers = loadUsers()
+            diff = list(filter(lambda x: x not in savedUsers, users))
+            if len(diff) > 0:
+                str = ", ".join([u["firstName"] + " " + u["lastName"] for u in diff])
+                msg = {
+                    "content": f"**{len(users)}** tickets vendus !\nDernier(s) acheteur(s) : **{str}**",
+                    "username": "HelloAsso",
+                    "avatar_url": "https://www.helloasso.com/blog/wp-content/uploads/2022/02/Circle-logo-ha-150x150.png",
+                }
+                sendWebhook(msg)
+                saveUsers(users)
+        case "list":
+            print(f"{len(users)} tickets vendus")
+            for user in users:
+                print(f"{user['firstName']} {user['lastName']}")
+        case _:
+            print("No args")
+
+
+if __name__ == "__main__":
+    main()
